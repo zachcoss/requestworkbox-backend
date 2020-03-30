b4fe7c7e8155d29428bf9b497941d0f1b8f42837
Zach <zacharycoss@gmail.com>
2020-03-30 10:48:09 -0500

    adding cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const http = require('http');
 const app = express();
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 const indexRouter = require('./routes/index');
 const connectionRouter = require('./routes/connection');
