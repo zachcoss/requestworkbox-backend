7a0a7ca008a16fea45a2fee4e4fce008d5ca3cd3
Zach <zacharycoss@gmail.com>
2020-09-26 20:21:57 -0500

    updating ports for elb

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 const http = require('http');
 const app = express();
-const port = process.env.PORT || '3000'
+const port = process.env.PORT
 
 app.set('port', port);
 
