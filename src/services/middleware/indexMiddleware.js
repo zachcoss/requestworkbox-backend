b94412696c15c2143f91418e67f19c672a87cf99
Zach <zacharycoss@gmail.com>
2020-11-25 16:18:24 -0600

    updating api key functionality

--- src/services/middleware/indexMiddleware.js
+++ src/services/middleware/indexMiddleware.js
@@ -16,9 +16,15 @@ module.exports = {
             if ((!req.user || !req.user.sub) && !req.headers['x-api-key']) {
                 return res.status(401).send('user not found')
             } else if (req.headers['x-api-key']) {
+
                 const uuid = req.headers['x-api-key']
                 const snippet = uuid.substring(0,8)
                 const token = await IndexSchema.Token.findOne({ snippet, active: true, })
+
+                if (!token || !token._id) {
+                    return res.status(401).send('token not found')
+                }
+
                 const validToken = await passwordHash.compare(uuid, token.hash)
                 
                 req.user = { sub: token.sub }
